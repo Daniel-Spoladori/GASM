90a95b3ad56e9ef61feaf6fe9729bfaec36e2b24
Daniel Spoladori <daniel.spoladori@gmail.com>
2024-12-31 15:36:28 -0300

    major changes

--- lexer.c
+++ lexer.c
@@ -9,10 +9,30 @@ const char* checkToken(char* element){
 			return (tokens[i].token);
 		}
 	}
-	return 1;
+	return NULL;
 }
 
-int main(){
+int main(int argc, char const *argv[]){
 
+    if (argc != 4) {
+      	 printf("\nERROR 002:\nInsuficient arguments to GASM \nUsage: <source file> <output file> <architecture flag>\n");
+      	return 2;
+    }
+
+	const char *source_file = argv[1];
+	const char *output_file = argv[2];
+	const char *arch_flag = argv[3];
+
+	FILE *file = fopen("fileDir", "r");
+	if (file){
+
+		printf("FILE");
+
+	}else{
+
+		printf("\nERROR 003: no file or data found\n");
+		return 3;
+		
+	}
 	return 0;
 }
