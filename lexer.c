744337aa2c6d2ca612064ec0627c20ed9a1ec83f
Daniel Spoladori <daniel.spoladori@gmail.com>
2025-01-10 20:13:06 -0300

    work in progress

--- lexer.c
+++ lexer.c
@@ -2,11 +2,14 @@
 #include "tokens.h"
 #include "lib.h"
 
-const char* checkToken(const char* element){
+const char* checkToken(const char *element){
 	for (int i = 0; i < arraySize(tokens); i++){
 		if (!strcmp(element, tokens[i].keyword)){
 			return (tokens[i].token);
 		}
 	}
 	return NULL;
+}
+const char* getParameters(const char *data,  const char *character ){
+
 }
