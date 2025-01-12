1a177156c6bc34e6b099b0935df5c5798d72a669
Daniel Spoladori <daniel.spoladori@gmail.com>
2025-01-12 17:09:14 -0300

    implemented error lib

--- errors.h
+++ errors.h
@@ -1,6 +1,21 @@
 #ifndef ERRORS_H
 	#define ERRORS_H
-	
-	#define
-	
+	typedef struct {
+		const char *errorMessage;
+		const int errorID;
+	} Errors;
+
+
+	#define CREATE_ERROR(name, message, id) \
+		static const Errors name = { message, id };
+
+	#define printError(inputError) \
+		(printf("\nERROR: %d\n%s\nLine:%d File:%s\n", \
+			(inputError).errorID, \
+			(inputError).errorMessage, \
+			__LINE__, \
+			__FILE__))
+
+	CREATE_ERROR(ERR_noFileOrData, "No file or data found", 300)
+	CREATE_ERROR(ERR_insufficientArg, "Insufficient arguments when callling GASM\nUsage: <source file> <output file> <architecture flag>", 500)
 #endif
